636a3dadbaf383e8510af3450dc5d3ba5f001eff
labsx <jrlabrador@revlv.com>
2023-09-04 12:14:43 -0400

    dashboard modify

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -28,46 +28,53 @@
             @endif
 
             <div class="max-w-7xl mx-auto p-6 lg:p-8">
-                <div class="flex justify-center">
+                <div class="">
+                    <div class="flex justify-center mt-6">
                         <form class="form-inline" action="/" method="GET">
                         <input class="form-control mr-sm-2 " type="text" name="search" placeholder="Search" aria-label="Search">
                         <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
                         </form>
-                </div>
-                
-                <div class="mt-16">
-                    <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
-                        @unless ($posts->isEmpty())
-                        @foreach ($posts as $post)
-                            <div class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
-                                <div>
-                                    <div class="h-16 w-16 bg-red-50 dark:bg-red-800/20 flex items-center justify-center rounded-full">
-                                        <img src="{{ asset('storage/' . $post->picture) }}" style="height:50px"/>
-                                    </div>
-                                        <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">{{$post->name}}</h2>
-                                            <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-                                                {{ $post->venue }}
-                                            </p>
-                                            <p class="mt-1 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-                                               Date:  {{\Carbon\Carbon::parse($post->date)->format('d/m/Y') }}
-                                               {{-- {{ \Carbon\Carbon::parse($post->date)->diffForHumans() }} --}}
-                                            </p>
-                                </div>
-                            </div>
-                         @endforeach
-                         @else
-                            <div class="text-danger">
-                                <p>No event found !</p>
-                            </div>
-                         @endunless
                     </div>
-                    
-                </div>
-                    <div class="mt-4 float-right">
+                   <div class="mt-5">
+                    <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8 ">
+                        
+
+                        @unless ($posts->isEmpty())
+                            @foreach ($posts as $post)
+                                        <div class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500 ">
+                                            <div>
+                                                <div class="h-20 w-50 bg-red-50 dark:bg-red-800/20 text-center rounded-full">                                                          
+                                                        <img src="{{ asset('storage/' . $post->picture) }}" style="" class="h-100" />
+                                                </div>
+
+                                                    <h2 class="mt-1 text-xl font-semibold text-gray-900 dark:text-white">
+                                                      {{$post->name}}</h2>
+                                                        <p class="mt-1 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+                                                            {{ $post->venue }} </p> 
+                                                        </p>
+                                                        <p class="mt-1 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+                                                            {{ $post->description }} </p> 
+                                                        </p>
+
+                                                        <p class="mt-1 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+                                                            {{ \Carbon\Carbon::parse($post->date)->diffForHumans() }}
+                                                        </p>
+                                            </div>
+                                        </div>
+                               
+                            @endforeach
+                         @else
+                        <div class="text-danger">
+                            <p>No event data found !.</p>
+                        </div>
+                        @endunless
+                    </div>
+                </div> 
+                 <div class="mt-4 float-right">
                         {{ $posts->links() }}
                     </div>
                 </div>
-            </div>
+            </div>  
         </div>
     </body>
 </html>
